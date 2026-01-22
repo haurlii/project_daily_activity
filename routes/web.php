5a3c6b38c8beab75cbf3fae49be41456677b4ad6
voicescretch <rendiapriliyan80@gmail.com>
2026-01-23 06:41:48 +0700

    full screen modal

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
